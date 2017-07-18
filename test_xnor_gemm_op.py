47b48ae70108b1ae2566a84812449c31a5b2ea4b
AngusG <premiumdriedfoods@gmail.com>
2017-07-18 15:47:45 -0400

    update test case

--- test_xnor_gemm_op.py
+++ test_xnor_gemm_op.py
@@ -6,12 +6,15 @@ class XnorGemmTest(tf.test.TestCase):
     def testXnorGemm(self):
         xnor_gemm_module = tf.load_op_library('./libs/xnor_gemm_kernel.so')
         with self.test_session():
-        	
-        	a = tf.constant([1, 0, 1, 0, 1, 0], shape=[2, 3])
-        	b = tf.constant([0, 1, 0, 1, 0, 1], shape=[3, 2])
+            
+            a = tf.constant([1, 0, 1, 0, 1, 0], shape=[2, 3])
+            b = tf.constant([0, 1, 0, 1, 0, 1], shape=[3, 2])
 
-            result = xnor_gemm_module.xnor_gemm(a,b)
-            self.assertAllEqual(result.eval(), [0, 2, 0, 1], shape=[2,2])
+            xnor_result = xnor_gemm_module.xnor_gemm(a,b)
+            base_result = tf.matmul(a,b)
+
+            #self.assertAllEqual(result.eval(), [0, 2, 0, 1], shape=[2,2])
+            self.assertAllEqual(xnor_result.eval(), base_result.eval())
 
 if __name__ == "__main__":
     tf.test.main()

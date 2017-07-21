1e7ba3ad34c63aa34917568cf615be1f6e863417
AngusG <premiumdriedfoods@gmail.com>
2017-07-21 16:53:35 -0400

    set N to smallest value

--- test_gemm_op.py
+++ test_gemm_op.py
@@ -7,7 +7,7 @@ class GemmTest(tf.test.TestCase):
         gemm_module = tf.load_op_library('./libs/gemm_op.so')
         with self.test_session():
 
-            N = 4096
+            N = 512
 
             a_float = tf.cast(2 * (tf.random_normal(shape=[N,N],seed=1).eval() > 0) - 1, tf.float32)
             b_float = tf.cast(2 * (tf.random_normal(shape=[N,N],seed=2).eval() > 0) - 1, tf.float32)

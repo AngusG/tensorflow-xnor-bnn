5d1c4bd2e9ab492a16c4ecb814975287a1c6477a
AngusG <premiumdriedfoods@gmail.com>
2017-07-25 14:02:00 -0400

    log events in descriptive sub-folders

--- binary_net.py
+++ binary_net.py
@@ -12,7 +12,7 @@ from gemm_op import xnor_gemm
 def SignTheano(x):
     return tf.subtract(tf.multiply(tf.cast(tf.greater_equal(x, tf.zeros(tf.shape(x))), tf.float32), 2.0), 1.0)
 '''
-
+BN_EPSILON = 1e-4
 
 @tf.RegisterGradient("QuantizeGrad")
 def quantize_grad(op, grad):
@@ -21,13 +21,13 @@ def quantize_grad(op, grad):
 
 class BinaryNet:
 
-    def __init__(self, binary, fast, n_hidden, x):
+    def __init__(self, binary, fast, n_hidden, x, batch_norm, phase):
         self.binary = binary
         self.fast = fast
         self.n_hidden = n_hidden
         self.input = x
         self.G = tf.get_default_graph()
-        self.dense_layers()
+        self.dense_layers(batch_norm, phase)
 
     def init_layer(self, name, n_inputs, n_outputs):
 
@@ -60,43 +60,74 @@ class BinaryNet:
             #E = tf.reduce_mean(tf.abs(x))
             # return tf.sign(x) * E
 
-    def dense_layers(self):
+    def dense_layers(self, batch_norm, phase):
 
         if self.binary:
 
-            with tf.name_scope('fc1_1b') as scope:
+            with tf.name_scope('fc1_b') as scope:
 
                 W_1 = self.init_layer('W_1', 784, self.n_hidden)
-                fc_1 = self.quantize(self.binary_tanh_unit(
+                fc1 = tf.cast(self.quantize(
+                    tf.matmul(self.input, tf.cast(self.quantize(W_1), tf.int32))), tf.float32)
+                '''
+                fc1 = self.quantize(self.binary_tanh_unit(
                     tf.matmul(self.input, self.quantize(W_1))))
-
-            with tf.name_scope('fc2_1b') as scope:
+                '''
+            with tf.name_scope('fc2_b') as scope:
 
                 W_2 = self.init_layer('W_2', self.n_hidden, self.n_hidden)
                 if self.fast:
-                    fc_2 = self.quantize(self.binary_tanh_unit(
-                        xnor_gemm(fc_1, self.quantize(W_2))))
+                    fc2 = self.quantize(self.binary_tanh_unit(
+                        xnor_gemm(fc1, self.quantize(W_2))))
                 else:
-                    fc_2 = self.quantize(self.binary_tanh_unit(
-                        tf.matmul(fc_1, self.quantize(W_2))))
+                    fc2 = self.quantize(self.binary_tanh_unit(
+                        tf.matmul(fc1, self.quantize(W_2))))
 
-            with tf.name_scope('fc3_1b') as scope:
+            with tf.name_scope('fc3_b') as scope:
 
-                W_3 = self.init_layer('W_3', self.n_hidden, 10)
-                self.output = tf.matmul(fc_2, self.quantize(W_3))
+                W_3 = self.init_layer('W_3', self.n_hidden, self.n_hidden)
+                if self.fast:
+                    fc3 = self.quantize(self.binary_tanh_unit(
+                        xnor_gemm(fc2, self.quantize(W_3))))
+                else:
+                    fc3 = self.quantize(self.binary_tanh_unit(
+                        tf.matmul(fc2, self.quantize(W_3))))
+
+            with tf.name_scope('fcout_b') as scope:
+
+                W_3 = self.init_layer('W_out', self.n_hidden, 10)
+                self.output = tf.matmul(fc2, self.quantize(W_3))
         else:
 
             with tf.name_scope('fc1_fp') as scope:
 
                 W_1 = self.init_layer('W_1', 784, self.n_hidden)
-                fc_1 = tf.nn.relu(tf.matmul(self.input, W_1))
+                self.layer_1_summ = tf.summary.histogram(name='layer_1_summ', values=W_1)
+                fc1 = tf.nn.relu(tf.matmul(self.input, W_1))
+                if batch_norm:
+                    fc1 = tf.contrib.layers.batch_norm(
+                        fc1, center=True, scale=True, epsilon=BN_EPSILON, is_training=phase)
 
             with tf.name_scope('fc2_fp') as scope:
 
                 W_2 = self.init_layer('W_2', self.n_hidden, self.n_hidden)
-                fc_2 = tf.nn.relu(tf.matmul(fc_1, W_2))
+                fc2 = tf.nn.relu(tf.matmul(fc1, W_2))
+                if batch_norm:
+                    fc2 = tf.contrib.layers.batch_norm(
+                        fc2, center=True, scale=True, epsilon=BN_EPSILON, is_training=phase)
 
             with tf.name_scope('fc3_fp') as scope:
 
-                W_3 = self.init_layer('W_3', self.n_hidden, 10)
-                self.output = tf.matmul(fc_2, W_3)
+                W_3 = self.init_layer('W_3', self.n_hidden, self.n_hidden)
+                fc3 = tf.nn.relu(tf.matmul(fc2, W_3))
+                if batch_norm:
+                    fc3 = tf.contrib.layers.batch_norm(
+                        fc3, center=True, scale=True, epsilon=BN_EPSILON, is_training=phase)
+
+            with tf.name_scope('fcout_fp') as scope:
+
+                W_out = self.init_layer('W_out', self.n_hidden, 10)
+                self.output = tf.matmul(fc3, W_out)
+                if batch_norm:
+                    self.output = tf.contrib.layers.batch_norm(
+                        self.output, center=True, scale=True, epsilon=BN_EPSILON, is_training=phase)
--- mnist_fc_bnn.py
+++ mnist_fc_bnn.py
@@ -12,6 +12,18 @@ from tensorflow.python import debug as tf_debug
 import tensorflow as tf
 from binary_net import BinaryNet
 
+BN_TRAIN_PHASE = 1
+BN_TEST_PHASE = 0
+
+def create_dir_if_not_exists(dir):
+    if not os.path.exists(dir):
+        dir += '/1'
+    else:
+        sub_dir = str(int(next(os.walk(dir))[1][-1]) + 1)
+        dir += '/' + sub_dir
+    os.makedirs(dir)
+    print('Logging to %s' % dir)
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -28,18 +40,26 @@ if __name__ == '__main__':
     parser.add_argument(
         '--n_hidden', help='number of hidden units', type=int, default=512)
     parser.add_argument(
+        '--batch_size', help='examples per mini-batch', type=int, default=100)
+    parser.add_argument(
+        '--max_steps', help='maximum training steps', type=int, default=1000)
+    parser.add_argument(
         '--binary', help="should weights and activations be constrained to -1, +1", action="store_true")
     parser.add_argument(
         '--fast', help="if binary flag is passed, determines if xnor_gemm cuda kernel is used to accelerate training, otherwise no effect", action="store_true")
+    parser.add_argument(
+        '--batch_norm', help="batch normalize activations", action="store_true")
     parser.add_argument(
         '--debug', help="run with tfdbg", action="store_true")
     args = parser.parse_args()
 
     # handle command line args
     if args.binary:
+        print("Using 1-bit weights and activations")
         binary = True
         sub_1 = '/bin/'
         if args.fast:
+            print("Using fast xnor_gemm kernel")
             fast = True
             sub_2 = 'xnor/'
         else:
@@ -51,67 +71,119 @@ if __name__ == '__main__':
         binary = False
         fast = False
 
-    #outpath = os.path.join(args.train_dir + sub_1 + sub_2, args.sub)
-    outpath = os.path.join(args.train_dir + sub_1 + sub_2)
+    log_path = args.train_dir + sub_1 + sub_2 + 'hid_' + str(args.n_hidden) + '/'
+    if args.batch_norm:
+        print("Using batch normalization")
+        batch_norm = True
+        alpha = 0.1
+        epsilon = 1e-4
+        log_path += 'batch_norm/'
+    else:
+        batch_norm = False
 
+    log_path += 'bs_' + str(args.batch_size)
+
+    #log_path = os.path.join(log_path, args.sub)
+    create_dir_if_not_exists(log_path)
+    '''
     # import data
     if binary:
-        mnist = input_data.read_data_sets(args.data_dir, dtype=tf.uint8, one_hot=True)
+        mnist = input_data.read_data_sets(
+            args.data_dir, dtype=tf.uint8, one_hot=True)
+        x = tf.placeholder(tf.int32, [None, 784])
     else:
-        mnist = input_data.read_data_sets(args.data_dir, dtype=tf.float32, one_hot=True)        
+        mnist = input_data.read_data_sets(
+            args.data_dir, dtype=tf.float32, one_hot=True)
+        x = tf.placeholder(tf.float32, [None, 784])
+
+    phase = tf.placeholder(tf.bool, name='phase')
 
     # create the model
-    x = tf.placeholder(tf.float32, [None, 784])
-    bnn = BinaryNet(binary, fast, args.n_hidden, x)
+    bnn = BinaryNet(binary, fast, args.n_hidden, x, batch_norm, phase)
     y = bnn.output
     y_ = tf.placeholder(tf.float32, [None, 10])
 
     # define loss and optimizer
     cross_entropy = tf.reduce_mean(
         tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-    train_step = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
+
+    # for batch-normalization
+    if batch_norm:
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            # ensures that we execute the update_ops before performing the
+            # train_op
+            train_op = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
+    else:
+        train_op = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
 
     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    sess = tf.InteractiveSession()
-    print("tf.InteractiveSession()")
-    tf.global_variables_initializer().run()
-    print("tf.global_variables_initializer().run()")
+    #sess = tf.InteractiveSession()
+    # tf.global_variables_initializer().run()
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
 
     if args.debug:
+        print("Using debug mode")
         sess = tf_debug.LocalCLIDebugWrapperSession(sess)
         sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
 
     # setup summary writer
-    summary_writer = tf.summary.FileWriter(outpath, sess.graph)
+    summary_writer = tf.summary.FileWriter(log_path, sess.graph)
     training_summary = tf.summary.scalar("train loss", cross_entropy)
     test_summary = tf.summary.scalar("test acc.", accuracy)
+    layer_1_summ = bnn.layer_1_summ
 
     # Train
-    for step in range(1000):
+    for step in range(args.max_steps):
 
-        batch_xs, batch_ys = mnist.train.next_batch(512)
-        __, loss, train_summ = sess.run([train_step, cross_entropy, training_summary],
-                                        feed_dict={x: batch_xs, y_: batch_ys})
+        batch_xs, batch_ys = mnist.train.next_batch(args.batch_size)
+
+        if binary:
+            __, loss, train_summ = sess.run([train_op, cross_entropy, training_summary],
+                                            feed_dict={x: batch_xs.astype('int32'), y_: batch_ys.astype('int32')})
+        else:
+            if batch_norm:
+                __, loss, train_summ, l1_summ = sess.run([train_op, cross_entropy, training_summary, layer_1_summ],
+                                                         feed_dict={x: batch_xs, y_: batch_ys, phase: BN_TRAIN_PHASE})
+            else:
+                __, loss, train_summ, l1_summ = sess.run([train_op, cross_entropy, training_summary, layer_1_summ],
+                                                         feed_dict={x: batch_xs, y_: batch_ys})
 
         if step % 100 == 0:
-
             # Test trained model
             if binary:
-                test_batch_xs, test_batch_ys = mnist.test.next_batch(512)
+                test_batch_xs, test_batch_ys = mnist.test.next_batch(
+                    args.batch_size)
                 test_acc, test_summ = sess.run([accuracy, test_summary], feed_dict={
-                    x: test_batch_xs, y_: test_batch_ys})
+                    x: test_batch_xs.astype('int32'), y_: test_batch_ys.astype('int32')})
             else:
-                test_acc, test_summ = sess.run([accuracy, test_summary], feed_dict={x: mnist.test.images,
-                                                                         y_: mnist.test.labels})
+                if batch_norm:
+                    test_acc, test_summ = sess.run([accuracy, test_summary], feed_dict={x: mnist.test.images,
+                                                                                        y_: mnist.test.labels, phase: BN_TEST_PHASE})
+                else:
+                    test_acc, test_summ = sess.run([accuracy, test_summary], feed_dict={x: mnist.test.images,
+                                                                                        y_: mnist.test.labels})
             print("step %d, loss = %.4f, test accuracy %.4f" %
                   (step, loss, test_acc))
 
+            summary_writer.add_summary(l1_summ, step)
             summary_writer.add_summary(train_summ, step)
             summary_writer.add_summary(test_summ, step)
             summary_writer.flush()
 
     # Test trained model
-    print("Final test accuracy %.4f" % (sess.run(accuracy, feed_dict={x: mnist.test.images,
-                                                                      y_: mnist.test.labels})))
+    if binary:
+        print("Final test accuracy %.4f" % (sess.run(accuracy, feed_dict={x: mnist.test.images.astype('int32'),
+                                                                          y_: mnist.test.labels.astype('int32')})))
+    else:
+        if batch_norm:
+            print("Final test accuracy %.4f" % (sess.run(accuracy, feed_dict={x: mnist.test.images,
+                                                                              y_: mnist.test.labels,
+                                                                              phase: BN_TEST_PHASE})))
+        else:
+            print("Final test accuracy %.4f" % (sess.run(accuracy, feed_dict={x: mnist.test.images,
+                                                                              y_: mnist.test.labels})))
+    '''                                                                              
